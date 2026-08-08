f25b7fadf5ab0d258518c621a26c12f15f44bcb6
Colton Voege <colton@getaleph.com>
2026-08-08 18:20:36 -0400

    plugins

--- nvim/config/init.lua
+++ nvim/config/init.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Core settings
 vim.opt.number = true         -- show line numbers
-vim.opt.relativenumber = true -- line numbers relative to cursor (great for motions like 5j)
+vim.opt.relativenumber = false -- line numbers relative to cursor (great for motions like 5j)
 vim.opt.expandtab = true      -- use spaces instead of tabs
 vim.opt.shiftwidth = 2        -- indent size
 vim.opt.tabstop = 2           -- tab width
@@ -28,4 +28,30 @@ vim.opt.signcolumn = "yes"    -- always show the sign column (avoids text shifti
 -- Plugin setup
 require("lazy").setup({
   -- plugins will go here
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   priority = 1000, -- load this before other plugins
+  --   config = function()
+  --     vim.cmd("colorscheme tokyonight")
+  --   end,
+    -- },
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "dark", -- try: dark, darker, cool, deep, warm, warmer
+      })
+      require("onedark").load()
+    end,
+  },
+
+  { "williamboman/mason.nvim", config = true },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+  },
+
+  { "neovim/nvim-lspconfig" },
 })
